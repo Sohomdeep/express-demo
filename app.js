d0b35d98b948e015ad6b3b51bddb8ccda344c3bb
Sohomdeep <sohomdeeppaul@gmail.com>
2022-05-10 21:08:10 +0530

    express sequelize setup

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
-
 const express = require('express');
+const sequelize = require('./util/database');
+
 const app = express();
 
 const server = http.createServer();
 
-server.listen(3000);
+sequelize
+   //.sync({ force: true })
+   .sync()
+  .then(result => {
+        console.log(result);
+        app.listen(3000);
+  })
+  .catch(err => {
+        console.log('DB Connection error');
+        console.log(err);
+  });
---
+++ controllers/user.js
@@ -0,0 +1,98 @@
+const Product = require('../models/user');
+
+exports.getAddUser = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
+
+exports.postAddUser = (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  req.user
+    .createUser({
+      name: name,
+      email: email
+    })
+    .then(result => {
+      // console.log(result);
+      console.log('Created User');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+/*exports.getEditUser = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  req.user
+    .getUsers({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postEditUser = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  Product.findByPk(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getUsers = (req, res, next) => {
+  req.user
+    .getProducts()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postDeleteUser = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};*/
